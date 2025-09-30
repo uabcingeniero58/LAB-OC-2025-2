26b240dd1cbdd1c0967644b8138420eb642401ea
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-09-30 17:50:42 +0000

    s

---
+++ P5/p5e.asm
@@ -0,0 +1,25 @@
+%include "../LIB/pc_io.inc"
+
+section	.text
+	global _start
+	
+_start:                   
+    ; Mostrar cadena original
+	mov edx, msg_original
+	call puts
+	
+	; Modificar 'z' a 'Z' 
+	mov ebx, msg - 25  ; base ajustada
+	mov edi, 25        ; índice
+	mov byte [ebx + edi], 'Z'
+	
+	; Mostrar cadena modificada
+	mov edx, msg
+	call puts
+
+	mov	eax, 1	    	; sys_exit
+	int	0x80        	
+
+section	.data
+msg_original db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
+msg db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
