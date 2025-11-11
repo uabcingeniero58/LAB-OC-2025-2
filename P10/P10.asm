528b337fe265b0cbb970b6ddec52fcb24e270d73
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-11-11 19:27:33 +0000

    p10

---
+++ P10/P10.asm
@@ -0,0 +1,12 @@
+section .text
+   global suma
+   global strlen
+   global getbit
+
+
+   global _start
+
+   _start:
+
+   suma:
+   
---
+++ P10/P10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+extern int suma (int a ,int b)
+extern int strlen(const char* str);
+extern int getbit (int num, int pos);
+
+int main(){
+  int resultado = suma(8, 1)
+  printf("suma: %d + %d = %d\n",resultado , int a , int b);
+
+  const char* texto = "hola UABC";
+  int longitud = strlen(texto);
+  printf("longitud de '%s': %d caracteres\n", texto, longitud);
+  
+  int numero = 45;
+  for(int i=0; i<8; i++){
+  printf("Bit %d de %d: %d\n", i, numero, getbit(numero, i));
+  }
+
+}
