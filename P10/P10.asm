04bce05b22b5db5d512e718f8789cb3a2cca03c7
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-11-11 19:53:27 +0000

    as12

--- P10/P10.asm
+++ P10/P10.asm
@@ -18,7 +18,32 @@ section .text
    ret
 
    strlen:
- 
+    push epb
+    mov ebp,esp
+    jump .dup1
+
+    .dup4:
+     mov eax,[ebp+8]
+     movzx eax, BYTE [eax]
+     mov edx,[ebp+12]
+     cmp al,dl
+     jne .dup2
+     mov eax,[epb+8]
+     jmp .dup3
+
+    .dup2:
+     add DWORD [ebp+8],1
+
+    .dup1:
+     mov eax,[ebp+8]
+     movzx eax,BYTE [eax]
+     test al,al
+     jne .dup4
+     mov eax,0
+
+    .dup3
+     pop epb
+     ret
 
    getbit:
    push ebp
