27bfae09bcf3a09f97d68883fd068e51ca07d19b
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-11-18 19:12:46 +0000

    sdxasvwe

--- P11/test_P11.c
+++ P11/test_P11.c
@@ -3,9 +3,9 @@
 
 // var = variable
 extern void pBin8b(uint8_t var);
-extern void PBin16b(uint16_t var);
-extern void PBin32b(uint32_t var);
-extern void PBin64b(uint64_t var);
+extern void pBin16b(uint16_t var);
+extern void pBin32b(uint32_t var);
+extern void pBin64b(uint64_t var);
 
 int main(void){
 
@@ -13,13 +13,13 @@ printf("Prueba de pBin8b con dato aleatorio : \n");
 pBin8b(32);
 
 printf("\nPrueba de pBin16b con dato aleatorio : \n");
-PBin16b(2048);
+pBin16b(2048);
 
 printf("\nPrueba de pBin32b con dato aleatorio : \n");
-PBin32b(414666);
+pBin32b(414666);
 
 printf("\nPrueba de pBin64b con dato aleatorio : \n");
-PBin64b(44073709551615ULL);
+pBin64b(44073709551615ULL);
 
 return 0;
 
