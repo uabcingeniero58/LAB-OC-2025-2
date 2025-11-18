50799a1183d42a1b48d5111be625ffce63351b87
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-11-18 18:40:14 +0000

    dsacaqdewq12312

--- P11/test_P11.c
+++ P11/test_P11.c
@@ -2,10 +2,10 @@
 #include <stdint.h>
 
 // var = variable
-extern void pBin8b (uint8_t var);
-extern void PBin16b (uint16_t var);
-extern void PBin32b (uint32_t var);
-extern void PBin64b (uint64_t var);
+extern void pBin8b(uint8_t var);
+extern void PBin16b(uint16_t var);
+extern void PBin32b(uint32_t var);
+extern void PBin64b(uint64_t var);
 
 int main(void){
 
