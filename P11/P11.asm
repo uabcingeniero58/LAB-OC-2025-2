adc8cfea0ead523212f793ac8d5cc0e22e4bf9d0
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-11-18 18:30:20 +0000

    aslea;s

--- P11/P11.asm
+++ P11/P11.asm
@@ -10,6 +10,47 @@ section .text
    _start:
 
    pBin8b:
+    push ebp
+    mov ebp, esp
+    push eax
+    push ebx
+    push ecx
+    
+    mov ebx, [ebp+8]
+    and ebx, 0xFF
+    mov cx, 8
+    mov edx, 0
+    
+.next:
+    mov al, '0'
+    inc edx
+    cmp edx, 4
+    jne .cont
+    push eax
+    mov al, '-'
+    call putchar
+    pop eax
+    mov edx, 0
+    
+.cont:
+    rol ebx, 1
+    jnc .zero
+    inc al
+    
+.zero:
+    call putchar
+    loop .next
+    
+    push eax
+    mov al, 10
+    call putchar
+    pop eax
+    
+    pop ecx
+    pop ebx
+    pop eax
+    pop ebp
+    ret
 
    pBin16b:
 
---
+++ P11/test_P11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// var = variable
+extern void pBin8b (uint8_t var);
+extern void PBin16b (uint16_t var);
+extern void PBin32b (uint32_t var);
+extern void PBin64b (uint64_t var)
+
+int main(void){
+
+printf("Prueba de pBin8b con dato aleatorio : \n");
+pBin8b(32);
+
+printf("\nPrueba de pBin16b con dato aleatorio : \n");
+pBin16b(4242);
+
+printf("\nPrueba de pBin32b con dato aleatorio : \n");
+pBin32b(414666);
+
+printf("\nPrueba de pBin64b con dato aleatorio : \n");
+pBin64b(18446744073709551615ULL);
+
+return 0;
+
+}
