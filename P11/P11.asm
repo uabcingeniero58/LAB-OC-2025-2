adcda6e6d0208f7c4e2e06cc8cc392150960c80b
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-11-18 18:13:21 +0000

    213421-2--2--2

---
+++ P11/P11.asm
@@ -0,0 +1,18 @@
+section .text
+   global pBin8b
+   global pBin16b
+   global pBin32b
+   global pBin64b
+
+
+   global _start
+
+   _start:
+
+   pBin8b:
+
+   pBin16b:
+
+   pBin32b:
+
+   pBin64b
